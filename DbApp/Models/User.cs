42ee2459c988fe5291d6328a9342a88af8336f19
baarnabas <37655660+baarnabas@users.noreply.github.com>
2021-06-16 14:01:46 +0200

    Add project files.

---
+++ DbApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DbApp.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public ICollection<Device> Devices{ get; set; }
+    }
+}
